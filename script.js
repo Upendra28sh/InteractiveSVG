fa4020ba2e314a3e41c1c490c6100ec69f6c99b9
Upendra Sharma <upendra861@gmail.com>
2018-07-23 20:17:26 +0530

    create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+var btns=document.querySelectorAll(".buttons button");
+var cat = document.getElementsByClassName("cat")[0];
+
+for(var i=0;i<btns.length;i++)
+{
+    btns[i].addEventListener("click",myfunction);
+}
+
+function myfunction()
+{
+    if(this.getAttribute("data-add"))
+    {
+        cat.classList.add(this.getAttribute("data-add"))
+    }
+
+    if(this.getAttribute("data-remove"))
+    {
+        cat.classList.remove(this.getAttribute("data-remove"))
+    }
+}
